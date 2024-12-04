236832d6466e6da4083dba3a87d348e06ccc79b2
Oleksandr Rapiy <rapiy.oleksandr@gmail.com>
2024-12-04 23:07:16 +0200

    add rest files

---
+++ src/check_if_exist/lib.rs
@@ -0,0 +1,35 @@
+﻿pub struct Solution;
+
+// LeetCode link: https://leetcode.com/problems/check-if-n-and-its-double-exist/
+impl Solution {
+    pub fn check_if_exist(arr: Vec<i32>) -> bool {
+        for i in 0..arr.len() {
+            for j in (0..arr.len()).rev() {
+                if i != j && arr[i] == (2 * arr[j]) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::Solution;
+    use rstest::rstest;
+
+    #[rstest]
+    fn check_if_exist_should_return_true() {
+        let result = Solution::check_if_exist(vec![10, 2, 5, 3]);
+
+        assert_eq!(result, true);
+    }
+
+    #[rstest]
+    fn check_if_exist_should_return_false() {
+        let result = Solution::check_if_exist(vec![-2, 0, 10, -19, 4, 6, -8]);
+
+        assert_eq!(result, false);
+    }
+}
---
+++ src/is_prefix_of_word/lib.rs
@@ -0,0 +1,30 @@
+﻿pub struct Solution;
+
+// LeetCode link: https://leetcode.com/problems/check-if-a-word-occurs-as-a-prefix-of-any-word-in-a-sentence/description/
+impl Solution {
+    pub fn is_prefix_of_word(sentence: String, search_word: String) -> i32 {
+        let words = sentence.split(' ');
+
+        for (index, word) in words.enumerate() {
+            if word.starts_with(search_word.as_str()) {
+                return (index + 1) as i32;
+            }
+        }
+        return -1;
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use rstest::rstest;
+
+    #[rstest]
+    #[case("i love eating burger", "burg", 4)]
+    fn is_prefix_of_word_return_index_of_prefix(#[case] sentence: &str, #[case] search_word: &str, #[case]expected_index: i32) {
+        let result = Solution::is_prefix_of_word(sentence.to_string(), search_word.to_string());
+
+        assert_eq!(result, expected_index);
+    }
+}
+
